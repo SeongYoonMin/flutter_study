655c9a37ffdc134599a08ea4b2f298e511c706af
SeongYoonMin <namo4862@naver.com>
2023-10-02 17:25:16 +0900

    Fix: add Iamges, input, button

--- STUDY.md
+++ STUDY.md
@@ -5,5 +5,17 @@
 Flutter 에서 const(상수)로 선언된 위젯은 새로고침을 할 필요가 없어서 성능상 이점이 있다.
 상황에 때라 const를 붙여주면 이득을 볼 수 있다.
 
+## WIDGET
+
 HOME : 화면에 보여줄곳
-Scaffold : 위젯으로 한 페이지의 특정 영역에 위젯을 쉽게 배치할 수 있도록 틀을 잡아주는 역활
+Scaffold : 한 페이지의 특정 영역에 위젯을 쉽게 배치할 수 있도록 틀을 잡아주는 역활
+Text : 텍스트위젯
+Column : children이라는 자식배열을 갖고, 해당 배열에 담긴 위젯들을 column정렬하여 보여준다.
+
+## 단축키
+
+ctrl + shift + r : 리팩토링
+
+## ISSUE
+
+I/O > Keyboard > Connet hardware Keyboard 체크
--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,47 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Text(
-          'text Flutter',
-          style: TextStyle(
-              fontSize: 35, fontWeight: FontWeight.bold, color: Colors.amber),
+        appBar: AppBar(
+          title: Text(
+            'Flutter Login',
+            style: TextStyle(
+                fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(
+                16), // all = 사방 , only = 특정 위치 top, bottom, left, right
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                    width: 81,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Email"),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Password"),
+                  obscureText: true,
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 40),
+                  child: ElevatedButton(onPressed: () {}, child: Text("login")),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
